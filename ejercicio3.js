562928ca70b8a6cc928faecc44cc96ff910d68c2
Santiago Colorado <130319819+Dasacoga@users.noreply.github.com>
2023-05-04 09:57:52 -0500

    ejercicio 3 aparentemente finiquitado

---
+++ ejercicio3.js
@@ -0,0 +1,11 @@
+//Parte 1
+const PI=3.1416
+console.log(`El valor de PI es: ${PI}`)
+
+//Parte 2
+
+const segenmin=60
+const mimenhor=60
+const horendia=24
+
+console.log("Un minuto tiene "+segenmin+" segundos, una hora tiene "+mimenhor+" minutos y un día tiene "+horendia+" horas")
