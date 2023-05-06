770647a519d44f589c2b26eccca12f50cc00988b
Santiago Colorado <130319819+Dasacoga@users.noreply.github.com>
2023-05-05 20:38:36 -0500

    ejercicio 4 terminado y 5 primer commit

---
+++ ejercicio4.js
@@ -0,0 +1,26 @@
+//Parte 1
+let cadenaTexto="Hola mundo!"
+console.log(cadenaTexto)
+
+let nombre="Santiago"
+cadenaTexto= "Hola!"+" Mi nombre es "+nombre
+console.log(cadenaTexto)
+
+let edad=23
+console.log(`${nombre} tiene ${edad} años`)
+
+let verdadero =true
+console.log(`${nombre} tiene ${edad} años`)
+
+let color
+console.log(`Mi color favorito es ${color}`)
+
+
+//Parte 2
+let Nombre ="Santiago"
+let Apellido="Colorado"
+let Edad=20
+let Cancionfav=undefined
+
+console.log("Hola!. Mi nombre es "+Nombre+" "+Apellido+" tengo "+Edad+" años y mi cancion favorira es ")
+
