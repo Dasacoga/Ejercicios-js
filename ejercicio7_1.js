fa693b63033062174cde461758ae7e19e9970f87
Santiago Colorado <130319819+Dasacoga@users.noreply.github.com>
2023-05-11 09:50:34 -0500

    ejercicio 7 terminado

---
+++ ejercicio7_1.js
@@ -0,0 +1,23 @@
+//parte 1
+var i=11
+while (i>1){
+    i--
+    console.log(i)
+}
+//parte 2
+for (var i=0;i<12;i=i+2){
+console.log(i)
+}
+//parte 3
+b=0
+for (i=0;i<6;i++){
+    b=b+i
+}
+console.log(b)
+//parte 4 tabla del 7 con while
+console.log("Esta es la tabla del 7")
+i=0
+while (i<10){
+    i++
+    console.log (7+"x"+i+"="+(7*i))
+}
