d96962f346ef8de45186a3188541b274e361a402
Santiago Colorado <130319819+Dasacoga@users.noreply.github.com>
2023-05-16 17:07:10 -0500

    ejercicio 11 parte 1

---
+++ ejercicio11.js
@@ -0,0 +1,28 @@
+console.log("-----------------Array(n).fill()----------------------")
+const a= Array(100).fill(false) 
+console.log(a)
+
+const colores =["Rojo", "Azul", "Gris"]
+console.log(colores)
+
+//Con el método push() se agrega un elemento en la ultima posicion del arreglo.
+
+console.log("-----------------push()----------------------")
+colores.push("Verde")
+console.log(colores)
+
+//Con el método pop() se elimina el ultimo elemento del arreglo.
+console.log("-----------------pop()----------------------")
+colores.pop()
+console.log(colores)
+
+//Con el metodo slice() se crea una copia del arreglo
+
+console.log("-----------------slice()----------------------")
+const copiaColores =colores.slice();
+console.log(copiaColores)
+
+//La propiedad lenght obtiene el tamaño del arreglo.
+
+console.log("-----------------lenght----------------------")
+console.log(colores.length)
