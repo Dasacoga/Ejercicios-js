8131d5a53027e1111c94809452d19a5bd1055af4
Santiago <santiagogarcia2711@gmail.com>
2023-05-22 11:15:40 -0500

    ejercicio 15 terminado

---
+++ ejercicio15_1.js
@@ -0,0 +1,12 @@
+const diassem=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+const [diauno,diados,diatres,diacuatro,diacinco,diaseis,diasiete]=diassem
+console.log(diauno,diados,diatres,diacuatro,diacinco,diaseis,diasiete)
+
+const mascota={
+    nombre: "Toby",
+    edad:8,
+    tamaño:"1.00",
+    color:"cafe"
+}
+const {nombre,edad,tamaño,color}=mascota
+console.log(nombre,edad,tamaño,color)
