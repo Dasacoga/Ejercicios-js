6d98c0d629dbc4563660add06cd018093ca1b2e3
Santiago <santiagogarcia2711@gmail.com>
2023-05-22 09:56:59 -0500

    ejercicio 13 terminado

--- ejercicio13_1.js
+++ ejercicio13_1.js
@@ -1,3 +1,4 @@
+//clse figura
 class figura {
     constructor(color){
         this.color=color;
@@ -7,6 +8,36 @@ class figura {
     }
 }
 
-const circulo= new figura("gris")
-console.log(circulo)
-circulo.saludar()
+const triangulo= new figura("gris")
+console.log(triangulo)
+triangulo.saludar()
+
+
+//clase cuadrado de figura
+class cuadrado extends figura{
+    constructor(color,altoyancho){
+        super(color)
+        this.altoyancho=altoyancho
+    }
+    saludar(){
+        console.log("Hola! soy un cuadrado. Mi color es "+this.color+" y mis medidas son "+this.altoyancho+"x"+this.altoyancho)
+    }
+}
+const ab=new cuadrado("azul",10)
+console.log(cuadrado)
+ab.saludar()
+
+
+//clase circulo de figura
+class circulo extends figura{
+    constructor(color,radio){
+        super(color)
+        this.radio=radio
+    }
+    saludar (){
+        console.log("Hola! soy un circulo. Mi color es "+this.color+" y mi radio es "+this.radio)
+    }
+}
+ const cir=new circulo("verde",4)
+ console.log(cir)
+ cir.saludar()
