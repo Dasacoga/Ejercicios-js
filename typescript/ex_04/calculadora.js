a4f4d2307486e3c1af57cd7402670f1d3504187c
Santiago <santiagogarcia2711@gmail.com>
2023-06-17 17:11:35 -0500

    ejercicio ts avanzado

---
+++ typescript/ex_04/calculadora.js
@@ -0,0 +1,15 @@
+"use strict";
+$(function () {
+    $(".suma").click(function () {
+        $("#resul").hide(); //Metodo qee esconde elemento seleccionado
+    });
+    $(".resta").click(function () {
+        $("#resul").show(); //Método que muestra el elemento seleccionado
+    });
+    $(".multiplicacion").click(function () {
+        $("#resul").hide(); //Metodo qee esconde elemento seleccionado
+    });
+    $(".division").click(function () {
+        $("#resul").show(); //Método que muestra el elemento seleccionado
+    });
+});
---
+++ typescript/ex_04/calculadora.ts
@@ -0,0 +1,17 @@
+
+$(function(){
+   
+    $(".suma").click(function(){ //Evento click realiza una accion al dar click en el elemento.
+        $("#resul").hide() //Metodo qee esconde elemento seleccionado
+    })
+    $(".resta").click(function(){
+        $("#resul").show() //Método que muestra el elemento seleccionado
+    })
+    $(".multiplicacion").click(function(){ //Evento click realiza una accion al dar click en el elemento.
+        $("#resul").hide() //Metodo qee esconde elemento seleccionado
+    })
+    $(".division").click(function(){
+        $("#resul").show() //Método que muestra el elemento seleccionado
+    })
+
+})
